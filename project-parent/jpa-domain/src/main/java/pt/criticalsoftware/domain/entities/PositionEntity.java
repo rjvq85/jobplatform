2f322bdebec573508701277290c279c2780b6887
Ricardo Quirino <ricardoquirino@me.com>
2015-07-31 17:40:10 +0100

    Position

--- project-parent/jpa-domain/src/main/java/pt/criticalsoftware/domain/entities/PositionEntity.java
+++ project-parent/jpa-domain/src/main/java/pt/criticalsoftware/domain/entities/PositionEntity.java
@@ -1,12 +1,16 @@
 package pt.criticalsoftware.domain.entities;
 
 import java.time.LocalDate;
+import java.util.Collection;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import pt.criticalsoftware.domain.entities.states.PositionState;
@@ -34,6 +38,30 @@ public class PositionEntity {
 	@Column(name="localizacao",nullable=false)
 	private String locale;
 	
+	@Column(name="estado",nullable=false)
+	@Enumerated(EnumType.STRING)
 	private PositionState state;
+	
+	@Column(name="empresa",nullable=false)
+	private String company;
+	
+	@Column(name="area_tecnica",nullable=false)
+	private String technicalArea;
+	
+	@Column(name="sla",nullable=false)
+	private String sla;
+	
+	@Column(name="vagas",nullable=false)
+	private Integer vacancies;
+	
+	@Column(name="responsavel",nullable=false)
+	@OneToOne
+	private UserEntity responsable;
+	
+	@Column(name="descricao",nullable=false)
+	private String description;
+	
+	@Column(name="canais_publicacao",nullable=false)
+	private Collection<String> adChannels;
 
 }
--- project-parent/jpa-domain/src/main/java/pt/criticalsoftware/domain/entities/states/PositionState.java
+++ project-parent/jpa-domain/src/main/java/pt/criticalsoftware/domain/entities/states/PositionState.java
@@ -14,8 +14,9 @@ public enum PositionState {
 	public String getName() {
 		return name;
 	}
-
-	public void setName(String name) {
-		this.name = name;
+	
+	@Override
+	public String toString() {
+		return getName();
 	}
 }

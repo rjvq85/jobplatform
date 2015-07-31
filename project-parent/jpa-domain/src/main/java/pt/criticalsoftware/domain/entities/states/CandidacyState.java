a6999b6d38ed46928b4921211ffaeb09ea0d23e6
emiliaribeiro <emiliaribei@gmail.com>
2015-07-31 17:58:27 +0100

    Estado Candidatura

---
+++ project-parent/jpa-domain/src/main/java/pt/criticalsoftware/domain/entities/states/CandidacyState.java
@@ -0,0 +1,25 @@
+package pt.criticalsoftware.domain.entities.states;
+
+public enum CandidacyState {
+
+	SUBMTIDA("Submtida"),
+	EM_ENTREVISTA("Em entrevista"),
+	EM_NEGOCIACAO("Em Negociacao"),
+	REJEITADA("Rejeitada"),
+	FECHADA("fechada");
+	
+	private String name;
+	
+	private CandidacyState(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	@Override
+	public String toString() {
+		return getName();
+	}
+}
